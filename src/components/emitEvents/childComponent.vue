f95603c05710c3ea068dfcb183fc04f94e934b26
HannesLinde <hanneslindenberg@posteo.de>
2022-04-12 08:41:23 +0200

    update emit components

--- src/components/emitEvents/childComponent.vue
+++ src/components/emitEvents/childComponent.vue
@@ -1,9 +1,34 @@
 <template>
-    <div>
+    <div class="child-container">
+      <h4>Child component is orange</h4>
         <button @click="$emit('deleteText', message, divisor)">Delete all text</button>
     </div>
 </template>
 <script setup lang="ts">
+import {defineEmits} from "vue";
+
 const message = "The child component lets you know that... "
 const divisor = 3;
+
+const emit = defineEmits<{
+  (e: 'deleteText', message: string, divisor: number): void
+}>()
 </script>
+<style lang="scss" scoped>
+.child-container {
+  margin: 1rem;
+  background-color: orange;
+
+  button {
+    padding: .5rem 1rem;
+    border-radius: .5rem;
+    border: 1px solid lightgrey;
+    background-color: #aaaaaa;
+    font-weight: bold;
+
+    &:hover {
+      background-color: #4fc08d;
+    }
+  }
+}
+</style>
--- src/components/emitEvents/parentComponent.vue
+++ src/components/emitEvents/parentComponent.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
+    <div style="background-color: lightblue">
+      <p>Parent component is lightblue</p>
         <p v-if="text">{{text}}</p>
         <p v-else>No text so far</p>
-        <input type="text" v-model="text" placeholder="enter some text" />
+        <input
+            type="text"
+            v-model="text"
+            placeholder="enter some text"
+            class="fancy-input"
+        />
         <child-component @deleteText="deleteNow"></child-component>
         <p>{{deletionNotification}}</p>
     </div>
@@ -19,3 +25,12 @@ const deleteNow = (message: string, divisor: number) => {
         setTimeout(()=> deletionNotification.value = '', 3000)
 }
 </script>
+<style lang="scss" scoped>
+.fancy-input {
+  text-align: center;
+  padding: .5rem 1rem;
+  margin: .5rem;
+  border: solid 2px lightgrey;
+  border-radius: 0.3rem
+}
+</style>
