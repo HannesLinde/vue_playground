f1ca8fc96e1df69484f8e47df2c740714dbea81a
HannesLinde <hanneslindenberg@posteo.de>
2022-04-14 13:23:07 +0200

    remove redirect from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,9 +25,14 @@ import SlotsExercise from '../views/SlotsExerciseMain.vue'
 import Examples from '../views/Examples.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {path: '/', redirect: '/home'},
+  // {path: '/', redirect: '/home'},
   {
     path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/',
     name: 'Home',
     component: Home
   },
